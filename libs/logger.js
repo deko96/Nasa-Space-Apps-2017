daaa7b7259678c2880267ae4ce5640f09a62e192
Dejan Bozhinoski <bozhinoski996@gmail.com>
2017-04-28 16:32:00 +0200

    Cheerio fix

--- libs/logger.js
+++ libs/logger.js
@@ -1,17 +1,17 @@
-var cheerio = require('cheerio');
+var chalk = require('chalk');
 
 var Logger = function() {
 
     this.success = function(message) {
-        return console.log('%s %s', cheerio.green('[OK]'), message);
+        return console.log('%s %s', chalk.green('[OK]'), message);
     };
 
     this.warning = function(message) {
-        return console.log('%s %s', cheerio.orange('[WARN]'), message);
+        return console.log('%s %s', chalk.orange('[WARN]'), message);
     }
 
     this.error = function(message) {
-        return console.log('%s %s', cheerio.red('[ERROR]'), message);
+        return console.log('%s %s', chalk.red('[ERROR]'), message);
     }
 };
 
