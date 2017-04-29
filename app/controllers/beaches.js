4a0751cd6cb86d098f1c94fbd13db70680974b7f
Dejan Bozhinoski <bozhinoski996@gmail.com>
2017-04-29 08:28:25 +0200

    Beaches Query

--- app/controllers/beaches.js
+++ app/controllers/beaches.js
@@ -1,22 +1,63 @@
 var googleAPI = require('../../libs/googleapi');
+var Logger = require('../../libs/logger');
 
 var BeachesController = function() {
 
     /**
+     * Get Beaches by Lat & Long
+     */
+    var getBeachesByCoords = function(lat, long, callback) {
+        console.log(lat, long);
+        return googleAPI.nearBySearch({
+            location: [lat, long],
+            types: "natural_feature",
+            radius: 100000,
+            keyword: 'beach OR плажа'
+        }, function(error, response) {
+
+            if (error) {
+                return Logger.error('Something went wrong inside "getBeaches" function.. Please try again!');
+            }
+
+            callback(response);
+
+        });
+
+    };
+
+    /**
      * Get Google Places Data
      */
-    var parseGoogleData = function(lat, long) {
-        console.log(googleAPI);
-        return googleAPI;
+    var getGoogleData = function(params, callback) {
+        if (params.query !== 'undefined') {
+            var searchOpt = {
+                query: params.query,
+                types: 'locality'
+            };
+            return googleAPI.textSearch(searchOpt, function(error, response) {
+                if (error) {
+                    return Logger.error('Something went wrong with the search..');
+                }
+                if (response.results.length > 0) {
+                    return getBeachesByCoords(response.results[0].geometry.location.lat, response.results[0].geometry.location.lng, function(data) {
+                        callback(data);
+                    });
+                } else {
+                    callback(null);
+                }
+            });
+        }
     };
 
     this.getBeaches = function(req, res, next) {
         var params = req.query;
 
-        if (!params.lat || !params.long) {
+        if (typeof params.query === 'undefined' && (typeof params.lat === 'undefined' || typeof params.long === 'undefined')) {
             return next();
         } else {
-            return res.send(parseGoogleData(params.lat, params.long));
+            return getGoogleData(params, function(data) {
+                res.json(data);
+            });
         }
     };
 
